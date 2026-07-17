a07d5476b705fa812e2a92839b8e1c330476cef8
Daniel Winston Mandela Tulung <danielwinstonmandela@mail.ugm.ac.id>
2026-07-17 12:18:10 +0700

    resolved whatsapp preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
+    process.env.NEXT_PUBLIC_BASE_URL ?? "https://penting-kkn-ugm.vercel.app",
   ),
   title: {
     default: `${strings.appName} — ${strings.appFullName}`,
